bd0cd57a3d16bfb0c497d44fac4fa3a96609cb37
AaqibBashirMir <dazzlingmustakeen@gmail.com>
2023-10-03 23:25:26 +0530

    done

---
+++ script.js
@@ -0,0 +1,14 @@
+let div1=document.getElementById("div1");
+div1.style.backgroundColor="yellow";
+div1.style.height="200px";
+div1.style.width="300px";
+div1.style.margin="20px";
+div1.style.padding="10px";
+
+
+
+let text=document.getElementById("para");
+text.style.fontSize="18px"
+text.style.fontweight="bold"
+text.style.color="blue"
+
